e154e573c98f44a31ea0733114e27aefbe8f3e8e
Ali Bakhtiari <ali.bakhtiarii@gmail.com>
2025-10-19 13:13:56 +0330

    Update clean.py

--- clean.py
+++ clean.py
@@ -7,10 +7,8 @@ def remove_specific_files(directory):
         file_path = os.path.join(directory, filename)
         # Check if the file is a .dxf or .jpg file and its name is not '1.dxf' or '1.jpg'
         if filename.endswith('.dxf') or filename.endswith('.jpg'):
-            if filename not in ['1.dxf', '1.jpg','clean-org.jpg','clean.jpg']:
-                # Remove the file
-                os.remove(file_path)
-                print(f"Removed: {file_path}")
+            os.remove(file_path)
+            print(f"Removed: {file_path}")
 
 
 remove_specific_files('/usr/src/app')
