a1cc61f5eb117b36fbf34cee8b0e24a435fd0736
Ali Bakhtiari <ali.bakhtiarii@gmail.com>
2025-10-19 13:14:20 +0330

    updates for production, using gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ cron
 # Start the Python application in the foreground
 # Using 'exec' ensures that the application becomes the main process (PID 1),
 # which allows it to receive signals correctly, like from 'docker stop'.
-exec python app.py
+exec gunicorn --workers 4 --bind 0.0.0.0:5000 app:app
